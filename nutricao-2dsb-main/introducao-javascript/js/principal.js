53aa8f00578a9f7b0d07182b1b1aa1437d9a9a22
v7LUCAS <130380998+v7LUCAS@users.noreply.github.com>
2023-09-06 11:23:55 -0300

    Add files via upload

---
+++ nutricao-2dsb-main/introducao-javascript/js/principal.js
@@ -0,0 +1,9 @@
+//----------------------------------------------------------------
+//daqui para baixo começa o js do site
+
+//buca no site pelo titulo e armazena ele dentro da variavel
+let titulo = document.querySelector(".titulo");
+//mostra no devtools o conteudo de texto da variavel titulo para fins de teste
+console.log(titulo.textContent);
+//altera o conteudo da variavel titulo para o texto desejado
+titulo.textContent = "Fica Grande Nutrição";
